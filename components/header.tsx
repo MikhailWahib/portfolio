2a2fe4ec73381abf50e061cc409d0a0f0bbfa394
MikhailWahib <mikhailwahib1@gmail.com>
2023-11-15 13:07:13 +0200

    Update resume download path

--- components/header.tsx
+++ components/header.tsx
@@ -52,7 +52,7 @@ const Header = () => {
 				<LinkBtn
 					download
 					title='Resume'
-					href='/Resume.docx'
+					href='/Resume.pdf'
 					padding='py-1 px-3'
 					className='ml-3 mb-5 md:mb-0'
 				/>
