6d348121ac8af28fab527d7f86a812f503956f4c
MikhailWahib <mikhailwahib1@gmail.com>
2024-04-16 04:34:14 +0200

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/sections/hero-section"
 import About from "@/components/sections/about-section"
 import Projects from "@/components/sections/projects-section"
-import OtherProjects from "@/components/sections/other-projects"
 import ContactSection from "@/components/sections/contact-section"
 
 export default function Home() {
