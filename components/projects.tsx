6feaeef45ca4042c12751693f862ea7da965e539
MikhailWahib <mikhailwahib1@gmail.com>
2024-04-17 01:37:40 +0200

    Refactor projects filtering

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,32 +1,10 @@
 "use client"
-import { useState } from "react"
-import { Project, Tags } from "@/types"
+import { Project } from "@/types"
 import ProjectsCard from "./projects-card"
-
-interface Props {
-	projects: Project[] | undefined
-}
-
-const Projects = ({ projects }: Props) => {
-	const [filter, setFilter] = useState<Tags | null>()
-	return (
-		<>
-			<ProjectsSectionNav setFilter={setFilter} />
-			<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[minmax(18rem,1fr)] place-content-center h-full gap-10 gap-y-10 mt-16'>
-				{filter
-					? projects
-							?.filter((project) => project.tags.includes(filter))
-							?.map((project, i) => <ProjectsCard key={i} data={project} />)
-					: projects?.map((project, i) => (
-							<ProjectsCard key={i} data={project} />
-					  ))}
-			</ul>
-		</>
-	)
-}
+import { useMemo, useState } from "react"
 
 const ProjectsSectionNav = ({ setFilter }: any) => {
-	const cats = ["all", "frontend", "backend", "fullstack", "other"]
+	const filters = ["all", "frontend", "backend", "fullstack", "other"]
 	const handleCLick = (c: string) => {
 		if (c === "all") {
 			setFilter(null)
@@ -36,16 +14,42 @@ const ProjectsSectionNav = ({ setFilter }: any) => {
 	}
 	return (
 		<nav className='relative top-8 flex justify-center gap-2 sm:gap-5'>
-			{cats.map((c) => (
+			{filters.map((f) => (
 				<button
+					key={f}
 					className='text-primary text-base sm:text-lg font-bold capitalize hover:underline'
-					onClick={() => handleCLick(c)}
+					onClick={() => handleCLick(f)}
 				>
-					{c}
+					{f}
 				</button>
 			))}
 		</nav>
 	)
 }
 
-export default Projects
+interface Props {
+	projects: Project[] | undefined
+}
+
+const projects = ({ projects }: Props) => {
+	const [filter, setFilter] = useState()
+
+	const filteredProjects = useMemo(() => {
+		if (!projects) return []
+		if (!filter) return projects
+		return projects.filter((project) => project.tags.includes(filter))
+	}, [projects, filter])
+
+	return (
+		<div className='max-w-[1800px] mx-auto'>
+			<ProjectsSectionNav setFilter={setFilter} />
+			<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[minmax(18rem,1fr)] place-content-center mt-16 gap-10'>
+				{filteredProjects?.map((project, i) => (
+					<ProjectsCard key={i} data={project} />
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default projects
