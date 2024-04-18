e789ea4f4ec02e657aa42072c8faaac7ff1e2d95
MikhailWahib <mikhailwahib1@gmail.com>
2024-04-18 07:25:49 +0200

    Fix imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Hero from "@/components/sections/hero-section"
-import About from "@/components/sections/about-section"
-import Projects from "@/components/sections/projects-section"
+import HeroSection from "@/components/sections/hero-section"
+import AboutSection from "@/components/sections/about-section"
+import ProjectsSection from "@/components/sections/projects-section"
 import ContactSection from "@/components/sections/contact-section"
 
 export default function Home() {
