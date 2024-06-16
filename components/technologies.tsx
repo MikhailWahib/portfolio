142a905de36cb6f1fbcfeae2650345769113483b
MikhailWahib <mikhailwahib1@gmail.com>
2024-06-16 05:44:58 +0200

    Update technologies: add Go

--- components/technologies.tsx
+++ components/technologies.tsx
@@ -1,4 +1,4 @@
-import { AiFillHtml5 } from "react-icons/ai"
+import { AiFillHtml5 } from 'react-icons/ai'
 import {
 	BiLogoCss3,
 	BiLogoTailwindCss,
@@ -8,40 +8,42 @@ import {
 	BiLogoNodejs,
 	BiLogoPostgresql,
 	BiLogoMongodb,
-} from "react-icons/bi"
-import { TbBrandCpp } from "react-icons/tb"
-import { FaVuejs, FaPython } from "react-icons/fa"
-import { TbBrandNextjs } from "react-icons/tb"
+} from 'react-icons/bi'
+import { TbBrandCpp } from 'react-icons/tb'
+import { FaVuejs, FaPython } from 'react-icons/fa'
+import { TbBrandNextjs } from 'react-icons/tb'
 import {
 	SiExpress,
 	SiRubyonrails,
 	SiFlask,
 	SiJest,
 	SiRedis,
-} from "react-icons/si"
-import { DiRuby } from "react-icons/di"
-import PopupSpan from "@/framer-components/popup-span"
+} from 'react-icons/si'
+import { DiRuby } from 'react-icons/di'
+import PopupSpan from '@/framer-components/popup-span'
+import { FaGolang } from 'react-icons/fa6'
 
 const skills = [
-	{ name: "C++", icon: <TbBrandCpp /> },
-	{ name: "JavaScript", icon: <BiLogoJavascript /> },
-	{ name: "TypeScript", icon: <BiLogoTypescript /> },
-	{ name: "Python", icon: <FaPython /> },
-	{ name: "Ruby", icon: <DiRuby /> },
-	{ name: "HTML", icon: <AiFillHtml5 /> },
-	{ name: "CSS", icon: <BiLogoCss3 /> },
-	{ name: "React", icon: <BiLogoReact /> },
-	{ name: "NextJS", icon: <TbBrandNextjs /> },
-	{ name: "VueJS", icon: <FaVuejs /> },
-	{ name: "TailwindCSS", icon: <BiLogoTailwindCss /> },
-	{ name: "NodeJS", icon: <BiLogoNodejs /> },
-	{ name: "ExpressJs", icon: <SiExpress /> },
-	{ name: "Ruby on Rails", icon: <SiRubyonrails /> },
-	{ name: "Flask", icon: <SiFlask /> },
-	{ name: "MongoDB", icon: <BiLogoMongodb /> },
-	{ name: "PostgreSQL", icon: <BiLogoPostgresql /> },
-	{ name: "Redis", icon: <SiRedis /> },
-	{ name: "Jest", icon: <SiJest /> },
+	{ name: 'C++', icon: <TbBrandCpp /> },
+	{ name: 'JavaScript', icon: <BiLogoJavascript /> },
+	{ name: 'TypeScript', icon: <BiLogoTypescript /> },
+	{ name: 'Python', icon: <FaPython /> },
+	{ name: 'Ruby', icon: <DiRuby /> },
+	{ name: 'go', icon: <FaGolang /> },
+	{ name: 'HTML', icon: <AiFillHtml5 /> },
+	{ name: 'CSS', icon: <BiLogoCss3 /> },
+	{ name: 'React', icon: <BiLogoReact /> },
+	{ name: 'NextJS', icon: <TbBrandNextjs /> },
+	{ name: 'VueJS', icon: <FaVuejs /> },
+	{ name: 'TailwindCSS', icon: <BiLogoTailwindCss /> },
+	{ name: 'NodeJS', icon: <BiLogoNodejs /> },
+	{ name: 'ExpressJs', icon: <SiExpress /> },
+	{ name: 'Ruby on Rails', icon: <SiRubyonrails /> },
+	{ name: 'Flask', icon: <SiFlask /> },
+	{ name: 'MongoDB', icon: <BiLogoMongodb /> },
+	{ name: 'PostgreSQL', icon: <BiLogoPostgresql /> },
+	{ name: 'Redis', icon: <SiRedis /> },
+	{ name: 'Jest', icon: <SiJest /> },
 ]
 
 const Technologies = () => {
