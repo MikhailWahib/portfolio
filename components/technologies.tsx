495950482facec1b2311bdb745018d0cb4e245ed
Mikhail <mikhailwahib1@gmail.com>
2025-11-17 21:32:00 +0200

    update technologies

--- components/technologies.tsx
+++ components/technologies.tsx
@@ -9,38 +9,36 @@ import {
 	BiLogoPostgresql,
 	BiLogoMongodb,
 } from 'react-icons/bi'
-import { TbBrandCpp } from 'react-icons/tb'
 import { FaVuejs, FaPython } from 'react-icons/fa'
 import { TbBrandNextjs } from 'react-icons/tb'
 import {
 	SiExpress,
 	SiRubyonrails,
 	SiFlask,
-	SiJest,
+	SiFastapi,
 	SiRedis,
 } from 'react-icons/si'
-import { DiRuby } from 'react-icons/di'
 import { FaGolang } from 'react-icons/fa6'
+import { FaRust } from 'react-icons/fa'
 
 const skills = [
 	{ name: 'JavaScript', icon: <BiLogoJavascript /> },
 	{ name: 'TypeScript', icon: <BiLogoTypescript /> },
+	{ name: 'Go', icon: <FaGolang /> },
+	{ name: 'Rust', icon: <FaRust /> },
 	{ name: 'Python', icon: <FaPython /> },
-	{ name: 'Ruby', icon: <DiRuby /> },
-	{ name: 'go', icon: <FaGolang /> },
+	{ name: 'NodeJS', icon: <BiLogoNodejs /> },
+	{ name: 'ExpressJs', icon: <SiExpress /> },
+	{ name: 'Ruby on Rails', icon: <SiRubyonrails /> },
+	{ name: 'PostgreSQL', icon: <BiLogoPostgresql /> },
+	{ name: 'MongoDB', icon: <BiLogoMongodb /> },
+	{ name: 'Redis', icon: <SiRedis /> },
 	{ name: 'HTML', icon: <AiFillHtml5 /> },
 	{ name: 'CSS', icon: <BiLogoCss3 /> },
 	{ name: 'React', icon: <BiLogoReact /> },
 	{ name: 'NextJS', icon: <TbBrandNextjs /> },
 	{ name: 'VueJS', icon: <FaVuejs /> },
 	{ name: 'TailwindCSS', icon: <BiLogoTailwindCss /> },
-	{ name: 'NodeJS', icon: <BiLogoNodejs /> },
-	{ name: 'ExpressJs', icon: <SiExpress /> },
-	{ name: 'Ruby on Rails', icon: <SiRubyonrails /> },
-	{ name: 'Flask', icon: <SiFlask /> },
-	{ name: 'MongoDB', icon: <BiLogoMongodb /> },
-	{ name: 'PostgreSQL', icon: <BiLogoPostgresql /> },
-	{ name: 'Redis', icon: <SiRedis /> },
 ]
 
 const Technologies = () => {
